be19dcbbfd5339e5d2d990db5524cca434951e1c
YuuShigetani <s2g4t1n2@gmail.com>
2013-12-05 21:55:53 +0900

    init

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+    public float moveSpeed = 1f;
+
+    private Transform frontCheck;
+
+    void Awake () {
+        frontCheck = transform.Find("frontCheck").transform;
+    }
+
+    void FixedUpdate () {
+        Collider2D[] frontHits = Physics2D.OverlapPointAll(frontCheck.position, 1);
+
+        foreach(Collider2D c in frontHits) {
+            if(c.tag == "Wall") {
+                Flip ();
+                break;
+            }
+        }
+
+        rigidbody2D.velocity = new Vector2(transform.localScale.x * moveSpeed, rigidbody2D.velocity.y);
+    }
+
+    public void Flip()
+    {
+        Vector3 enemyScale = transform.localScale;
+        enemyScale.x *= -1;
+        transform.localScale = enemyScale;
+    }
+}
